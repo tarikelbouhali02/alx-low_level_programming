70270dd99e5dca788a4da2b14329d58ed61ab683
tarikelbouhali02 <tarikbh99@gmail.com>
2023-07-13 17:55:29 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
-
-/**
+/** 
  *
- * Main – Entry point
- *
- * Return: Always 0 (Success)
- */
-Int main(void)
-	
+ *main - Entry point 
+ * 
+ *Return: Always 0 (Success) 
+ */ 
+int main(void) 
 {
-        Puts(“\”Programming is like building a multilingual puzzle”);
-
-        Return (0);
-
-}}
+  puts("\"Programming is like building a multilingual puzzle"); 
+  return (0);
+}
