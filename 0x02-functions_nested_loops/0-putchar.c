7ca332adbeb9dcf9ec1d8089a7bcb6de9b2a55d2
tarikelbouhali02 <tarikbh99@gmail.com>
2023-07-18 17:04:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-=======
-
-#include "main.h"
- /**
->>>>>>> c04617e85301bc679d5faebbca849bd5472ecf6f
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+
+printf("_putchar\n");
+
 return (0);
 }
